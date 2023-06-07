df663e23043f53d55df01989153ee5cb221fb583
Anurag Tiwari <anuragtiwari@Anurags-MacBook-Air.local>
2023-06-07 23:18:27 +0530

    created json for product and offer

--- index.ts
+++ index.ts
@@ -2,18 +2,11 @@ import { ProductRepository } from "./src/repository/product-repository";
 import { Checkout } from "./src/service/checkout";
 import { PricingRule } from "./src/service/pricing-rule";
 import { convertProductsJsonToProduct, convertProductOfferJsonToProductOffers } from "./src/utils";
+import productsDetails from './products.json';
+import offersDetail from './offer.json';
 
-const offer = [
-    { sku: "atv", minimumQuantity: 3, freeQuantity: 1 },
-    { sku: "ipd", minimumQuantity: 5, bulkPrice: 499.99 }
-]
-
-const productsJson = [
-    {sku: "ipd", name: "Ipad", price: 549.99},
-    {sku: "mbp", name: "MacBook Pro", price: 1399.99},
-    {sku: "atv", name: "Apple TV", price: 109.50},
-    {sku: "vga", name: "VGA adapter", price: 30.00}
-];
+const offer = offersDetail;
+const productsJson = productsDetails;
 
 const productOffers = convertProductOfferJsonToProductOffers(offer);
 const products = convertProductsJsonToProduct(productsJson);
